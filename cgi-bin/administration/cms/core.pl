97c27d02e3b9d33366e544434eafb7d28130e895
izemskov <pascal.ilya@gmail.com>
2024-04-26 19:22:15 +0300

    CMS

---
+++ cgi-bin/administration/cms/core.pl
@@ -0,0 +1,208 @@
+﻿use strict;
+
+our $CGIBIN_PATH;
+our $MOD;
+our $SUB;
+our $DWFilter;
+our $DWDB;
+
+require "$CGIBIN_PATH/$MOD/$SUB.pl";
+
+our $ModulTableName;
+
+my $action = $DWFilter->GetParamFilterLatinDigetDash("action");
+
+if ($SUB eq 'users') {
+    if ($action eq 'add') {
+        print &TemplateMainHeader;
+        print &AddEditElem('saveadd');
+        print &TemplateMainFooter;
+    }
+    elsif ($action eq 'saveadd') {
+        my $form = &AddEditGetFormData;
+        my %form = %$form;
+
+        my ($Errors, $form) = &AddEditCheckError(\%form);
+        %form = %$form;
+
+        if ($Errors eq '') {
+            &SaveAddElem(\%form);
+        }
+        else {
+            print &TemplateMainHeader;
+            print &AddEditElem('saveadd', $Errors, \%form);
+            print &TemplateMainFooter;
+        }
+    }
+    elsif ($action eq 'edit') {
+        my $form = &EditGetDBData;
+        my %form = %$form;
+
+        print &TemplateMainHeader;
+        print &AddEditElem('saveedit', '', \%form);
+        print &TemplateMainFooter;
+    }
+    elsif ($action eq 'saveedit') {
+        my $form = &AddEditGetFormData;
+        my %form = %$form;
+
+        $form{id} = $DWFilter->GetParamFilterDiget("id");
+
+        if ($form{id} ne '') {
+            my ($Errors, $form) = &AddEditCheckError(\%form);
+            %form = %$form;
+
+            if ($Errors eq '') {
+                &SaveEditElem(\%form);
+            }
+            else {
+                print &TemplateMainHeader;
+                print &AddEditElem('saveedit', $Errors, \%form);
+                print &TemplateMainFooter;
+            }
+        }
+        else {
+            print &TemplateMainHeader;
+            print &ShowDataAddElem;
+            print &ShowDataSimpleStructure('');
+            print &TemplateMainFooter;
+        }
+    }
+    elsif ($action eq 'change') {
+        my $form = &ShowDataGetFormData;
+        my %form = %$form;
+
+        my ($Errors, $form) = &ShowDataCheckError(\%form);
+        %form = %$form;
+
+        if ($Errors eq '') {
+            &ShowDataChange(\%form);
+        }
+        else {
+            print &TemplateMainHeader;
+
+            print &ShowDataAddElem;
+            print &ShowDataSimpleStructure($Errors);
+
+            print &TemplateMainFooter;
+        }
+    }
+    elsif ($action eq 'delete') {
+        &DeleteSimpleStructure;
+        print &ShowDataSimpleStructure('');
+    }
+    else {
+        print &TemplateMainHeader;
+        print &ShowDataAddElem;
+        print &ShowDataSimpleStructure('');
+        print &TemplateMainFooter;
+    }
+}
+else {
+    if ($action eq 'add') {
+        print &TemplateMainHeader;
+        print &AddEditElem('saveadd');
+        print &TemplateMainFooter;
+    }
+    elsif ($action eq 'saveadd') {
+        my $form = &AddEditGetFormData;
+        my %form = %$form;
+
+        my ($Errors, $form) = &AddEditCheckError(\%form);
+        %form = %$form;
+
+        if (($form{modul} ne '') && ($form{sub_modul} ne '')) {
+            my ($Count) = $DWDB->QuerySelectRowDB(qq{SELECT count(*) FROM }.$DWDB->GetMysqlTableName().$ModulTableName.qq{ WHERE modul=? AND sub_modul=?}, $form{modul}, $form{sub_modul});
+            if ($Count > 0) {
+                $Errors .= "<li>Данные модуль и подмодуль уже существуют</li>";
+            }
+        }
+
+        if ($Errors eq '') {
+            &SaveAddElem(\%form);
+        }
+        else {
+            print &TemplateMainHeader;
+            print &AddEditElem('saveadd', $Errors, \%form);
+            print &TemplateMainFooter;
+        }
+    }
+    elsif ($action eq 'edit') {
+        my $form = &EditGetDBData;
+        my %form = %$form;
+
+        print &TemplateMainHeader;
+        print &AddEditElem('saveedit', '', \%form);
+        print &TemplateMainFooter;
+    }
+    elsif ($action eq 'saveedit') {
+        my $form = &AddEditGetFormData;
+        my %form = %$form;
+
+        $form{id} = $DWFilter->GetParamFilterDiget("id");
+
+        if ($form{id} ne '') {
+            my ($Errors, $form) = &AddEditCheckError(\%form);
+            %form = %$form;
+
+            if (($form{modul} ne '') && ($form{sub_modul} ne '')) {
+                my ($OldModul, $OldSubModul) = $DWDB->QuerySelectRowDB(qq{SELECT modul, sub_modul FROM }.$DWDB->GetMysqlTableName().$ModulTableName.qq{ WHERE id=?}, $form{id});
+
+                if (($form{modul} ne $OldModul) || ($form{sub_modul} ne $OldSubModul)) {
+                    my ($Count) = $DWDB->QuerySelectRowDB(qq{SELECT count(*) FROM }.$DWDB->GetMysqlTableName().$ModulTableName.qq{ WHERE modul=? AND sub_modul=?}, $form{modul}, $form{sub_modul});
+                    if ($Count > 0) {
+                        $Errors .= "<li>Данные модуль и подмодуль уже существуют</li>";
+                    }
+                }
+            }
+
+            if ($Errors eq '') {
+                &SaveEditElem(\%form);
+            }
+            else {
+                print &TemplateMainHeader;
+                print &AddEditElem('saveedit', $Errors, \%form);
+                print &TemplateMainFooter;
+            }
+        }
+        else {
+            print &TemplateMainHeader;
+            print &ShowDataAddElem;
+            print &ShowDataParentStructure('');
+            print &TemplateMainFooter;
+        }
+    }
+    elsif ($action eq 'change') {
+        my $form = &ShowDataGetFormData;
+        my %form = %$form;
+
+        my ($Errors, $form) = &ShowDataCheckError(\%form);
+        %form = %$form;
+
+        if ($Errors eq '') {
+            &ShowDataChange(\%form);
+        }
+        else {
+            print &TemplateMainHeader;
+
+            print &ShowDataAddElem;
+            print &ShowDataParentStructure($Errors);
+
+            print &TemplateMainFooter;
+        }
+    }
+    elsif ($action eq 'delete') {
+        &DeleteParentStructure;
+        print &ShowDataParentStructure('');
+    }
+    elsif ($action eq 'delete_file') {
+        &DeleteModulFile;
+    }
+    else {
+        print &TemplateMainHeader;
+        print &ShowDataAddElem;
+        print &ShowDataParentStructure('');
+        print &TemplateMainFooter;
+    }
+}
+1;
---
+++ cgi-bin/administration/cms/moduls.pl
@@ -0,0 +1,164 @@
+﻿use strict;
+
+=item
+Тип модуля - Древовидный (core_parent)
+=cut
+
+our $HTDOCS_PATH;
+
+our $ModulName = 'Модули';
+our $ModulTableName = 'cms_struct';
+
+=item
+Show
+=cut
+# Поля выводимые при общем просмотре модуля
+our @ModulShowFields = (
+    'id',
+    'name',
+    'order_content'
+);
+# Тип полей в общем выводе
+our @ModulShowType = (
+    'text',
+    'input',
+    'input'
+);
+
+# Output
+our @ModulShowHeader = (
+    'ID',
+    'Название',
+    'Порядок'
+);
+our @ModulShowHeaderWidth = (
+    '40',
+    '100%',
+    '80'
+);
+our @ModulShowHeaderAlign = (
+    'center',
+    'left',
+    'center'
+);
+
+# Recurs output
+our $ModulShowRecursPaddingValue = 20;
+our @ModulShowRecursPadding = (
+    'nopadding',
+    'padding',
+    'nopadding'
+);
+
+# Filters
+our @ModulShowFieldsFilter = (
+    'none',
+    'htmlspecialchars',
+    'digits'
+);
+our @ModulShowFieldsFilterResult = (
+    'notempty',
+    'notempty',
+    'notempty'
+);
+
+# Delete files
+our @ModulShowDeleteFileField = (
+    'picture'
+);
+our @ModulShowDeleteFileResize = (
+    'small'
+);
+
+# Languages
+our @ModulShowLanguagesField = (
+    'none',
+    'none',
+    'none'
+);
+
+our $ModulShowChangeAll = 'yes';
+our $ModulShowIsEdit = 'yes';
+our $ModulShowIsDelete = 'yes';
+
+our $ModulAddIsDeleteFile = 'yes';
+
+our $ModulShowOrderBy = ' ORDER BY order_content, name ';
+
+our $ModulShowDeleteMessage = 'Вы уверены, что хотите удалить данный раздел?';
+our $ModulFileDeleteMessage = 'Вы уверены, что хотите удалить данный файл?';
+
+our $ModulUploadDirectory = "$HTDOCS_PATH/files/administration/moduls";
+our $ModulUploadDirectoryRel = "/files/administration/moduls";
+
+=item
+Add
+=cut
+# Fields
+our @ModulAddFields = (
+    'name',
+    'parent_id',
+    'modul',
+    'sub_modul',
+    'picture',
+    'order_content'
+);
+
+our @ModulAddFieldType = (
+    'input',
+    'select_parent',
+    'input',
+    'input',
+    'file',
+    'input'
+);
+
+# Output
+our @ModulAddFieldName = (
+    'Название',
+    'Расположение',
+    'Модуль',
+    'Под модуль',
+    'Картинка',
+    'Порядок'
+);
+
+# Filters
+our @ModulAddFieldsFilter = (
+    'htmlspecialchars',
+    'digits',
+    'latindigits',
+    'latindigits',
+    'picture',
+    'digits'
+);
+our @ModulAddFieldsFilterResult = (
+    'notempty',
+    'notempty',
+    'notempty',
+    'notempty',
+    'none',
+    'notempty'
+);
+
+# Resize
+our @ModulAddPictureResize = (
+    '',
+    '',
+    '',
+    '',
+    'small#50#',
+    ''
+);
+
+# Languages
+our @ModulAddLanguagesField = (
+    'none',
+    'none',
+    'none',
+    'none',
+    'none',
+    'none'
+);
+
+1;

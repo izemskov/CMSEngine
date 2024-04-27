84a40ddec94340adf8147512cac571c50cdf5b53
izemskov <pascal.ilya@gmail.com>
2024-04-27 14:13:01 +0300

    seo

---
+++ cgi-bin/administration/seo/core.pl
@@ -0,0 +1,99 @@
+﻿use strict;
+
+our $CGIBIN_PATH;
+our $MOD;
+our $SUB;
+our $DWFilter;
+our $DWDB;
+
+require "$CGIBIN_PATH/$MOD/$SUB.pl";
+
+our $ModulTableName;
+
+my $action = $DWFilter->GetParamFilterLatinDigetDash("action");
+
+if ($action eq 'add') {
+    print &TemplateMainHeader;
+    print &AddEditElem('saveadd');
+    print &TemplateMainFooter;
+}
+elsif ($action eq 'saveadd') {
+    my $form = &AddEditGetFormData;
+    my %form = %$form;
+
+    my ($Errors, $form) = &AddEditCheckError(\%form);
+    %form = %$form;
+
+    if ($form{translitname} ne '') {
+        my ($Count) = $DWDB->QuerySelectRowDB(qq{SELECT count(*) FROM }.$DWDB->GetMysqlTableName().$ModulTableName.qq{ WHERE translitname=?}, $form{translitname});
+        if ($Count > 0) {
+            $Errors .= "<li>Заголовок с таким кодом уже существует</li>";
+        }
+    }
+
+    if ($Errors eq '') {
+        &SaveAddElem(\%form);
+    }
+    else {
+        print &TemplateMainHeader;
+        print &AddEditElem('saveadd', $Errors, \%form);
+        print &TemplateMainFooter;
+    }
+}
+elsif ($action eq 'edit') {
+    my $form = &EditGetDBData;
+    my %form = %$form;
+
+    print &TemplateMainHeader;
+    print &AddEditElem('saveedit', '', \%form);
+    print &TemplateMainFooter;
+}
+elsif ($action eq 'saveedit') {
+    my $form = &AddEditGetFormData;
+    my %form = %$form;
+
+    $form{id} = $DWFilter->GetParamFilterDiget("id");
+
+    if ($form{id} ne '') {
+        my ($Errors, $form) = &AddEditCheckError(\%form);
+        %form = %$form;
+
+        if ($form{translitname} ne '') {
+            my ($OldSymbolLink) = $DWDB->QuerySelectRowDB(qq{SELECT translitname FROM }.$DWDB->GetMysqlTableName().$ModulTableName.qq{ WHERE id=?}, $form{id});
+
+            if ($form{translitname} ne $OldSymbolLink) {
+                my ($Count) = $DWDB->QuerySelectRowDB(qq{SELECT count(*) FROM }.$DWDB->GetMysqlTableName().$ModulTableName.qq{ WHERE translitname=?}, $form{translitname});
+                if ($Count > 0) {
+                    $Errors .= "<li>Заголовок с таким кодом уже существует</li>";
+                }
+            }
+        }
+
+        if ($Errors eq '') {
+            &SaveEditElem(\%form);
+        }
+        else {
+            print &TemplateMainHeader;
+            print &AddEditElem('saveedit', $Errors, \%form);
+            print &TemplateMainFooter;
+        }
+    }
+    else {
+        print &TemplateMainHeader;
+        print &ShowDataAddElem;
+        print &ShowDataSimpleStructure('');
+        print &TemplateMainFooter;
+    }
+}
+elsif ($action eq 'delete') {
+    &DeleteSimpleStructure;
+    print &ShowDataSimpleStructure('');
+}
+else {
+    print &TemplateMainHeader;
+    print &ShowDataAddElem;
+    print &ShowDataSimpleStructure('');
+    print &TemplateMainFooter;
+}
+
+1;
---
+++ cgi-bin/administration/seo/titles.pl
@@ -0,0 +1,138 @@
+﻿use strict;
+
+=item
+Тип модуля - Обычный (core_simple)
+=cut
+
+our $ModulName = 'Заголовки страниц';
+our $ModulTableName = 'seo_titles';
+
+# Поля выводимые при общем просмотре модуля
+our @ModulShowFields = (
+    'name',
+    'translitname',
+    'meta_title'
+);
+
+# Тип полей в общем выводе
+our @ModulShowType = (
+    'text',
+    'text',
+    'text'
+);
+
+# Output
+our @ModulShowHeader = (
+    'Название',
+    'Код',
+    'Заголовок страницы'
+);
+our @ModulShowHeaderWidth = (
+    '150',
+    '150',
+    '100%'
+);
+our @ModulShowHeaderAlign = (
+    'center',
+    'center',
+    'left'
+);
+
+# Filters
+our @ModulShowFieldsFilter = (
+    'none',
+    'none',
+    'none'
+);
+our @ModulShowFieldsFilterResult = (
+    'notempty',
+    'notempty',
+    'notempty'
+);
+
+# Delete files
+our @ModulShowDeleteFileField = (
+);
+our @ModulShowDeleteFileResize = (
+);
+
+# Languages
+our @ModulShowLanguagesField = (
+    'none',
+    'none',
+    'lang'
+);
+
+our $ModulShowChangeAll = 'no';
+our $ModulShowIsEdit = 'yes';
+our $ModulShowIsDelete = 'yes';
+
+our $ModulAddIsDeleteFile = 'no';
+
+our $ModulShowOrderBy = ' ORDER BY translitname ';
+
+our $ModulShowDeleteMessage = 'Вы уверены, что хотите удалить данный заголовок?';
+our $ModulFileDeleteMessage = '';
+
+our $ModulUploadDirectory = "";
+our $ModulUploadDirectoryRel = "";
+
+=item
+Add
+=cut
+# Fields
+our @ModulAddFields = (
+    'name',
+    'translitname',
+    'meta_title',
+    'meta_description',
+    'meta_keywords'
+);
+
+our @ModulAddFieldType = (
+    'input',
+    'input',
+    'textarea',
+    'textarea',
+    'textarea'
+);
+
+# Output
+our @ModulAddFieldName = (
+    'Название',
+    'Название на транслите',
+    'Мета-тег заголовки страницы',
+    'Мета-тег описание страницы',
+    'Мета-тег ключевые слова'
+);
+
+# Filters
+our @ModulAddFieldsFilter = (
+    'htmlspecialchars',
+    'latindigitsdash',
+    'htmlspecialchars',
+    'htmlspecialchars',
+    'htmlspecialchars'
+);
+our @ModulAddFieldsFilterResult = (
+    'notempty',
+    'notempty',
+    'none',
+    'none',
+    'none'
+);
+
+# Resize
+our @ModulAddPictureResize = (
+);
+
+# Languages
+our @ModulAddLanguagesField = (
+    'none',
+    'none',
+    'lang',
+    'lang',
+    'lang'
+);
+
+1;

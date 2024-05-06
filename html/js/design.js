e20e29a1ba7632e4bde901df3b2f584edaaec9c5
izemskov <pascal.ilya@gmail.com>
2024-05-06 13:33:55 +0300

    outter js

--- html/js/design.js
+++ html/js/design.js
@@ -1,22 +1,12 @@
 $(document).ready(
-	function () {			
-		$('.fancybox').fancybox({						
-			autoCenter: false,
-			helpers: {
-				overlay: {
-					fixed: false
-				}
-			}
-		});
-	}
+    function () {
+        $('.fancybox').fancybox({
+            autoCenter: false,
+            helpers: {
+                overlay: {
+                    fixed: false
+                }
+            }
+        });
+    }
 );
-
-function dowload_counter(file_name) {
-    var str = "/cgi-bin/scripts/download_counter.pl?filename=" + file_name;
-    $.ajax({
-        url: str
-    });
-
-    return true;
-}
-
